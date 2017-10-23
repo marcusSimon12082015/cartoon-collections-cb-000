05267349fbb5ac9bc61985822b169594e5676f60
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-10-23 15:39:06 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,6 @@ def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.each{|value|
-    if array.include?(value)
+    array.include?(value)
   }
 end
